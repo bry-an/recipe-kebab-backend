42fdb5b0c003dd506c2b52976573cf8b6f96d2d6
Bryan Yunis <bryany@booj.com>
2021-05-15 08:26:18 -0600

    add helmet

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from "cors"
+import helmet from "helmet"
 const app = express()
+app.use(helmet())
 app.use(cors())
 import {getPairings} from "./scraper.js"
 
